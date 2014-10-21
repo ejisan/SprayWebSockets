51cdeaa92e7d174c82fa2edcfdbecd8b7df7499f
Ryo Ejima <eji.san@mac.com>
2014-10-21 20:34:06 +0900

    Update

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object Build extends sbt.Build{
       Defaults.defaultSettings ++ Seq(
       organization  := "com.example",
       version       := "0.1",
-      scalaVersion  := "2.10.2",
+      scalaVersion  := "2.11.2",
 
       resolvers ++= Seq(
         "typesafe repo"      at "http://repo.typesafe.com/typesafe/releases/",
@@ -18,10 +18,10 @@ object Build extends sbt.Build{
         "spray nightly"      at "http://nightlies.spray.io/"
       ),
       libraryDependencies ++= Seq(
-        "io.spray"            %   "spray-can"     % "1.2-RC3",
-        "com.typesafe.akka"   %%  "akka-actor"    % "2.2.3",
-        "com.typesafe.akka"   %%  "akka-testkit"  % "2.2.3" % "test",
-        "org.scalatest"       % "scalatest_2.10" % "2.0" % "test"
+        "io.spray"            %%  "spray-can"     % "1.3.2",
+        "com.typesafe.akka"   %%  "akka-actor"    % "2.3.6",
+        "com.typesafe.akka"   %%  "akka-testkit"  % "2.3.6" % "test",
+        "org.scalatest"       %% "scalatest"      % "2.1.6" % "test"
       )
     )
   )
